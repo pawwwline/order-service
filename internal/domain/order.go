caa858ae42d8d42554a14e15a855ad79c333d29b
pawwwline <k.paulieee@gmail.com>
2025-08-06 18:59:35 +0500

    feat(domain): add order model

--- internal/domain/order.go
+++ internal/domain/order.go
@@ -1,1 +1,61 @@
 package domain
+
+import "time"
+
+type Order struct {
+	OrderUID    string
+	TrackNumber string
+	Entry       string
+	Delivery    Delivery
+	Payment     Payment
+	Items       []Item
+	Meta        Meta
+}
+
+type Delivery struct {
+	Name    string
+	Phone   string
+	Zip     string
+	City    string
+	Address string
+	Region  string
+	Email   string
+}
+
+type Payment struct {
+	Transaction  string
+	RequestID    string
+	Currency     string
+	Provider     string
+	Amount       int
+	PaymentDt    int
+	Bank         string
+	DeliveryCost int
+	GoodsTotal   int
+	CustomFee    int
+}
+
+type Item struct {
+	ChrtID      int
+	TrackNumber string
+	Price       int
+	Rid         string
+	Name        string
+	Sale        int
+	Size        string
+	TotalPrice  int
+	NmID        int
+	Brand       string
+	Status      int
+}
+
+type Meta struct {
+	Locale            string
+	InternalSignature string
+	CustomerID        string
+	DeliveryService   string
+	Shardkey          string
+	SmID              int
+	DateCreated       time.Time
+	OofShard          string
+}
