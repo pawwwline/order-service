341599e5f435796d7af9e1fd62d716bb8723c47b
pawwwline <k.paulieee@gmail.com>
2025-08-15 12:00:33 +0500

    feat(db): add migration for Orders tables

---
+++ db/migrations/20250815050550_create_order_entity.sql
@@ -0,0 +1,73 @@
+-- +goose Up
+-- +goose StatementBegin
+CREATE TABLE orders (
+    id SERIAL PRIMARY KEY,
+    order_uid VARCHAR(50) NOT NULL UNIQUE,
+    track_number VARCHAR(50),
+    entry VARCHAR(50),
+    customer_id VARCHAR(50),
+    delivery_service VARCHAR(50),
+    date_created TIMESTAMP DEFAULT now(),
+    date_updated TIMESTAMP DEFAULT now(),
+    locale VARCHAR(10),
+    internal_signature VARCHAR(255),
+    shardkey VARCHAR(10),
+    sm_id INT,
+    oof_shard VARCHAR(10)
+);
+
+CREATE TABLE deliveries (
+    id SERIAL PRIMARY KEY,
+    order_id INT REFERENCES orders(id) ON DELETE CASCADE,
+    name VARCHAR(100),
+    phone VARCHAR(30),
+    zip VARCHAR(20),
+    city VARCHAR(50),
+    address VARCHAR(255),
+    region VARCHAR(50),
+    email VARCHAR(100)
+);
+
+CREATE TABLE payments (
+    id SERIAL PRIMARY KEY,
+    order_id INT REFERENCES orders(id) ON DELETE CASCADE,
+    transaction VARCHAR(100),
+    request_id VARCHAR(100),
+    provider VARCHAR(20),
+    amount INT,
+    payment_dt INT,
+    bank VARCHAR(20),
+    delivery_cost INT,
+    goods_total INT,
+    custom_fee INT
+);
+
+CREATE TABLE order_items (
+    id SERIAL PRIMARY KEY,
+    order_id INT REFERENCES orders(id) ON DELETE CASCADE,
+    chrt_id INT,
+    track_number VARCHAR(100),
+    price INT,
+    rid VARCHAR(100),
+    name VARCHAR(100),
+    sale INT,
+    size VARCHAR(20),
+    total_price INT,
+    nm_id INT,
+    brand VARCHAR(100),
+    status INT
+);
+
+
+
+
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+SELECT 'down SQL query';
+DROP TABLE IF EXISTS order_items;
+DROP TABLE IF EXISTS payments;
+DROP TABLE IF EXISTS deliveries;
+DROP TABLE IF EXISTS orders;
+-- +goose StatementEnd
