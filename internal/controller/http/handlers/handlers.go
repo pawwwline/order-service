d6b962350ff2b6249dd78dab2c52e0c3e967745c
pawwwline <k.paulieee@gmail.com>
2025-08-20 16:20:14 +0500

    refactor(http): prevent error propagation

--- internal/controller/http/handlers/handlers.go
+++ internal/controller/http/handlers/handlers.go
@@ -25,41 +25,55 @@ func (h *HTTPHandler) RegisterRoutes(r *chi.Mux) {
 }
 
 func (h *HTTPHandler) GetOrderHandler(w http.ResponseWriter, r *http.Request) {
-	uid := chi.URLParam(r, "uuid")
+	uid := chi.URLParam(r, "uid")
 	if uid == "" {
 		http.Error(w, "uuid required", http.StatusBadRequest)
+
 		return
 	}
 
 	order, err := h.service.GetOrder(r.Context(), uid)
-	orderDTO := orderToResponse(order)
 	if err != nil {
 		switch {
 		case errors.Is(err, domain.ErrInvalidState):
-			http.Error(w, err.Error(), http.StatusBadRequest)
+
+			http.Error(w, "invalid state", http.StatusBadRequest)
 		case errors.Is(err, repo.ErrNotFound):
+
 			http.Error(w, err.Error(), http.StatusNotFound)
 		default:
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
+
+			http.Error(w, "internal server error", http.StatusInternalServerError)
 		}
 
-		w.Header().Set("Content-Type", "application/json")
-		json.NewEncoder(w).Encode(orderDTO)
+		return
+	}
+
+	orderDTO := orderToResponse(order)
+	w.Header().Set("Content-Type", "application/json")
+	err = json.NewEncoder(w).Encode(orderDTO)
+	if err != nil {
+
+		return
 	}
 }
 
 func orderToResponse(order *domain.Order) dto.OrderResponse {
+	if order == nil {
+		return dto.OrderResponse{} // или можно возвращать ошибку
+	}
 	items := make([]dto.ItemResponse, len(order.Items))
 	for i, item := range order.Items {
 		items[i] = dto.ItemResponse{
-			Name:       item.Name,
-			Price:      item.Price,
-			Sale:       item.Sale,
-			Size:       item.Size,
-			TotalPrice: item.TotalPrice,
-			Brand:      item.Brand,
-			Status:     item.Status,
+			ChrtID:      item.ChrtID,
+			TrackNumber: item.TrackNumber,
+			Name:        item.Name,
+			Price:       item.Price,
+			Sale:        item.Sale,
+			Size:        item.Size,
+			TotalPrice:  item.TotalPrice,
+			Brand:       item.Brand,
+			Status:      item.Status,
 		}
 	}
 
